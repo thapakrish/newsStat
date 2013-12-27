9e3d608de1c75aea755d29fda64464963387f6fa
Krishna Thapa <thapakrish@gmail.com>
2013-12-27 10:56:59 -0600

    frequency count, and some improvements on regex

--- getStory.pl
+++ getStory.pl
@@ -1,10 +1,16 @@
-#use strict;
-#user warnings;
+use strict;
+use warnings;
 use LWP::Simple;
+use Benchmark;
+
+my $start = new Benchmark;
 
 open(MYFILE, '>>data.txt');
-for ($num = 10000; $num < 10001; $num++) {
-$webpage=get "http://archives.myrepublica.com/portal/index.php?action=news_details&news_id=$num";
+for (my $num = 9999; $num < 10015; $num++) {
+my $webpage=get "http://archives.myrepublica.com/portal/index.php?action=news_details&news_id=$num";
+
+print "Grabbing story # ", $num, "\n";
+
 
 #Get the year, month, and day of the story
 my ($year, $month,$day) = $webpage =~ /(\d{4})-(\d{2})-(\d{2})/igs;
@@ -12,16 +18,21 @@ my ($year, $month,$day) = $webpage =~ /(\d{4})-(\d{2})-(\d{2})/igs;
 
 #Get heading
 $webpage =~ /bodynewsheading\"\>(.*?)\</igs;
-$heading = $1;
+my $heading = $1;
 $heading =~ s/^\s+|\s+$//g; 
 
 #Get content
-$webpage =~ /\<span style\=\"font-weight(.*?)\<\/div/igs;
-$content=$1;
-$content =~/\>(.*?)\,/;
-$originate = $1;
-$content =~ s/(.*?)\>\:(.*?)/\2/g;
-$content =~ s/(\<br\/\>)|(\<br\/)|(\<\/p\>) | (\d)//g;
+$webpage =~ /\<span style\=\"font-weight\: bold\;\">(.*?)\<\/p/igs;
+my $content=$1;
+#strip story origin
+$content =~ s/(.*?)\,(.*?)\>\:(.*?)/$3/;
+
+#strip line breaks,numbers, and others
+#$content =~ s/(\<br\/\>)|(\<br\/)|(\<\/p\>) | (\d)//g;
+$content =~ s/(\<(.*?)\>)|(\<br\/)|(\d)//g;
+
+
+#print $content, "\n";
 
 print MYFILE $content . "\n";
 #print $content . "\n";
@@ -29,4 +40,6 @@ print MYFILE $content . "\n";
 
 }
 close(MYFILE);
-
+my $end = new Benchmark;
+my $diff = timediff($end,$start);
+print "Time taken was ", timestr($diff, 'all'), " seconds";
--- makeDict.pl
+++ makeDict.pl
@@ -1,7 +1,11 @@
+#use POSIX;
+use Data::Dumper;
 #build up a dictionary
 @words=();
 @single=();
 %word_count =();
+$word_count=0;
+
 
 while (chomp ($line =<>)){
     @fields = split(/\s+/, $line);
@@ -14,17 +18,40 @@ while (chomp ($line =<>)){
 	    $word= lc($1);		    	    
 	    if (exists($dict{$word})) {
 		$dict{$word}++;
+			    $word_count +=1;
 	    }
 	else {
 	    $dict{$word} = 1;
+	    $word_count +=1;
 	}	
 	}
     }
-    
-    
-#sort, print wordcount    
-    foreach (sort { ($dict{$b} cmp $dict{$a}) || ($a cmp $b) } keys %dict) 
-	{
-    print "$_: $dict{$_}\n";
-	}
+
+
+#get the percentage
+foreach my $key (keys %dict) {
+	$percent=int($dict{$key} / $word_count * 100);
+	if($percent>1){
+		$word_freq{$key} = $percent;
+		}
 }
+	
+}
+
+#sort, print wordcount    
+#    foreach (sort { ($dict{$b} cmp $dict{$a}) || ($a cmp $b) } keys %dict) 
+#	{
+#    print "$_: $dict{$_}\n";
+#	}
+	
+	
+#sort, print wordcount from word_freq    
+    foreach (sort { ($word_freq{$b} cmp $word_freq{$a}) || ($a cmp $b) } keys %word_freq) 
+	{
+	    print "$_: $word_freq{$_}\n";
+	}
+
+#much easier this way, without sorting:
+#print Dumper(\%word_freq);	
+
+#print $word_count, "\n";	
